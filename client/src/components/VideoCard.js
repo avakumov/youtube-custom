bf8638cbdf6f05ac5b94e88b19f2d1f1b4bc38b8
Vladimir <avakdev@gmail.com>
2021-01-31 18:51:41 +0400

    feat(frontend): Added Home page

---
+++ client/src/components/VideoCard.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import Avatar from "../styles/Avatar"
+import Wrapper from "../styles/VideoCard"
+
+function VideoCard({ video, hideAvatar, noUsername }) {
+  return (
+    <Wrapper>
+      <Link to={`/watch/${video.id}`}>
+        <img className="thumb" src={video.thumbnail} alt={video.title} />
+      </Link>
+      <div className="video-info-container">
+        <div className="channel-avatar">
+          {!hideAvatar && (
+            <Avatar
+              style={{ marginRight: "0.8rem" }}
+              src={video.user.avatar}
+              alt={`${video.user.username}'s channel avatar`}
+            />
+          )}
+        </div>
+        <div className="video-info">
+          <Link to={`/watch/${video.id}`}>
+            <h4 className="truncate">{video.title}</h4>
+          </Link>
+          {!noUsername && (
+            <Link to={`/channel/${video.user.id}`}>
+              <span className="secondary">{video.user.username}</span>
+            </Link>
+          )}
+          <p className="secondary leading-4">
+            <span>{video.views} views</span> <span>•</span>{" "}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default VideoCard
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,27 @@
+import ErrorMessage from "../components/ErrorMessage"
+import VideoCard from "../components/VideoCard"
 import React from "react"
+import { useQuery } from "react-query"
 import HomeSkeleton from "../skeletons/HomeSkeleton"
+import { client } from "../utils/api-client"
+import Wrapper from "../styles/Home"
+import VideoGrid from "../styles/VideoGrid"
 
 function Home() {
-  return <HomeSkeleton />
+  const { data: videos, isSuccess, isLoading, isError, error } = useQuery("Home", () =>
+    client.get("/videos").then((res) => res.data.videos)
+  )
+
+  if (isLoading) return <HomeSkeleton />
+  if (isError) return <ErrorMessage error={error} />
+
+  return (
+    <Wrapper>
+      <VideoGrid>
+        {isSuccess ? videos.map((video) => <VideoCard key={video.id} video={video} />) : null}
+      </VideoGrid>
+    </Wrapper>
+  )
 }
 
 export default Home
