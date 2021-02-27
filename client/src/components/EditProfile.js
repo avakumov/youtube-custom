5b4a73a5358765d0a97bdca2b679dffd13d1153a
Vladimir <avakdev@gmail.com>
2021-02-27 16:11:26 +0400

    Added edit profile

---
+++ client/src/components/EditProfile.js
@@ -0,0 +1,23 @@
+// @ts-nocheck
+import React from "react"
+import Button from "../styles/Button"
+import Wrapper from "../styles/EditProfile"
+import EditProfileModal from "./EditProfileModal"
+
+function EditProfile({ profile }) {
+  const [showModal, setShowModal] = React.useState(false)
+  const closeModal = () => setShowModal(false)
+  return (
+    <>
+      <Wrapper>
+        <div>
+          <Button onClick={() => setShowModal(true)} grey>
+            Edit Profile
+          </Button>
+        </div>
+      </Wrapper>
+      {showModal && <EditProfileModal profile={profile} closeModal={closeModal} />}
+    </>
+  )
+}
+export default EditProfile
